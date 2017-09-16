1fc3bc3b1c1591e047e908c90a28c784aad3733d
jurkiewicz <jzdzisio@wp.pl>
2017-09-16 14:46:37 +0200

    rysowanie paletek, przesuwanie za pomocą klawiszy, menu, podsumowanie, odbijanie piłki, naprawianie bugów

--- main.js
+++ main.js
@@ -3,18 +3,36 @@ document.addEventListener("DOMContentLoaded", function(){
     function draw() {
 
         //zakończenie gry
-        if (prawy === 21 || lewy === 21) {
+        if (prawy === 21) {
             clearInterval(play);
+            c.clearRect(0, 0, 800, 494.44);
+            body.style.cursor = 'default';
+            end.style.visibility = 'visible';
+            end.innerHTML = 'Wygrał gracz z prawej!'
+            end.addEventListener('click', function(){
+                location.reload();
+            });
+        } else if (lewy === 21) {
+            clearInterval(play);
+            c.clearRect(0, 0, 800, 494.44);
+            body.style.cursor = 'default';
+            end.style.visibility = 'visible';
+            end.innerHTML = 'Wygrał gracz z lewej!'
+            end.addEventListener('click', function(){
+                location.reload();
+            });
         }
 
         //czyścimy obszar canvasu
         c.clearRect(0, 0, 800, 494.44);
 
         //naliczanie punktów i rozpoczynanie nowej rundy
-        if (ballX < 0) {
-            ballX = 145;
+        if (ballX + 10 < 0) {
+            ballX = 151;
             ballY = Math.random()*140;
-            stepX = -stepX;
+            stepX = 0.1;
+            stepY = 0.1;
+            znakY = Math.random();
             document.getElementById('prawy').innerHTML = ++prawy;
             if (prawy === 10) {
                 document.getElementById('prawy').style.left = '685px';
@@ -22,27 +40,75 @@ document.addEventListener("DOMContentLoaded", function(){
             if (znakY <= 0.5) {
                 stepY = -stepY
             }
-        } else if(ballX >= 300-10){
-            ballX = 145;
+        } else if(ballX >= 300){
+            ballX = 138.5;
             ballY = Math.random()*140;
-            stepX = -stepX;
+            stepX = -0.1;
+            stepY = 0.1;
+            var znakY = Math.random();
             document.getElementById('lewy').innerHTML = ++lewy;
             if (znakY <= 0.5) {
                 stepY = -stepY
             }
         }
 
+        //rysujemy piłkę w nowym miejscu
+        c.fillRect(ballX, ballY, 10, 10);
+
         //odbicie od krawędzi
         if (ballY < 0 || ballY >= 150-10) {
             stepY = -stepY;
         }
+
+        //odbicie od paletki
+        if (ballX <= 34 && ballX >= 30 && ((ballY + 10 >= LY || ballY >= LY) && (ballY <= LY + 20 || ballY + 10 <= LY + 20))) {
+            stepX -= 0.05;
+            if (stepY < 0) {
+                stepY -= 0.05;
+            } else {
+                stepY += 0.05;
+            }
+            stepX = -stepX;
+        }
+        if (ballX + 10 >= 266 && ballX + 10 <= 270 && ((ballY + 10 >= RY || ballY >= RY) && (ballY <= RY + 20 || ballY + 10 <= RY + 20))) {
+            stepX += 0.05;
+            if (stepY < 0) {
+                stepY -= 0.05;
+            } else {
+                stepY += 0.05;
+            }
+            stepX = -stepX;
+        }
         
-        //do x i y gwiazdki dodajemy przesuniecie
+        //do x i y piłki dodajemy przesuniecie
         ballX += stepX;
         ballY += stepY;
 
-        //rysujemy gwiazdkę w nowym miejscu
-        c.fillRect(ballX, ballY, 10, 10);
+        //przesunięcie paletek
+        if (LY <= 0 && LP == -2) {
+            LP = 0;
+            LY = 0;
+        } else if (LY >= 130 && LP == 2) {
+            LP = 0;
+            LY = 130;
+        } else {
+            LY += LP;
+            LP = 0;
+        }
+        if (RY <= 0 && RP == -2) {
+            RP = 0;
+            RY = 0;
+        } else if (RY >= 130 && RP == 2) {
+            RP = 0;
+            RY = 130;
+        } else {
+            RY += RP;
+            RP = 0;
+        }
+
+        //rysowanie paletek
+        c.fillRect(30, LY, 4, 20);
+        c.fillRect(266, RY, 4, 20);
 
         //linia środkowa
         for (var i = 0; i < 494.44;) {
@@ -54,15 +120,21 @@ document.addEventListener("DOMContentLoaded", function(){
     //punkty
     var lewy = 0;
     var prawy = 0;
+
+    //paletki
+    var LY = 65;
+    var RY = 65;
+    var LP = 0;
+    var RP = 0;
         
     //x i y piłki
     var middle = 148;
     var ballX = 145;
     var ballY = 75;
 
-    //przemieszczenie piłki
-    var stepX = 1;
-    var stepY = 1;
+    //przemieszczenie piłki i losowanie na początku gry
+    var stepX = 0.1;
+    var stepY = 0.1;
     var znakY = Math.random();
     if (znakY <= 0.5) {
         stepY = -stepY
@@ -72,9 +144,41 @@ document.addEventListener("DOMContentLoaded", function(){
         stepX = -stepX
     }
 
+    var body = document.getElementsByTagName('body')[0];
+
+    var end = document.getElementById('end');
+
+
     var table = document.getElementById('table');
     var c = table.getContext('2d');
     c.fillStyle = "#ffffff";
 
-    var play = setInterval(draw, 10);
+    window.addEventListener('keydown', function(event) {
+      switch (event.keyCode) {
+        case 65: // a
+          LP = -2;
+        break;
+     
+        case 90: // z
+          LP = 2;
+        break;
+     
+        case 75: // k
+          RP = -2;
+        break;
+     
+        case 77: // m
+          RP = 2;
+        break;
+      }
+    });
+
+    var play;
+    
+    body.addEventListener('click', function x(){
+        document.getElementById('menu').style.visibility = 'hidden';
+        body.style.cursor = 'none';
+        play = setInterval(draw, 1);
+        body.removeEventListener('click', x);
+    });
 });
